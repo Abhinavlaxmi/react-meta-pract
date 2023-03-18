4411c2b33f40ae2888570eb0c8f286d211b2220f
Abhinavlaxmi <jaiswalabhinav12@gmail.com>
2023-03-18 16:41:25 +0530

    created calculator and form with validations

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+// import Calculator from './components/Calculator/Calculator';
+import Registration from './components/Forms/Create-account/Registration';
 // import Counter from './components/ContextAPI/Counter';
 // import MealsList from './components/ContextAPI/MealsList';
 // import Provider from './components/ContextAPI/Provider';
-import Reducer from './components/Reducer/Reducer';
+// import Reducer from './components/Reducer/Reducer';
 
 function App() {
   return (
@@ -11,7 +13,9 @@ function App() {
       <MealsList />
       <Counter />
       </Provider> */}
-      <Reducer />
+      {/* <Reducer /> */}
+      {/* <Calculator /> */}
+      <Registration />
     </div>
   );
 }
